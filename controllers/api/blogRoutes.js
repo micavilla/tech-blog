82f11513992708ceec76fb49792041648ad33de3
micavilla <micaella.villa1@gmail.com>
2023-08-09 21:55:51 -0500

    started frontend and table for comments

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [{ model: Comment }]
+    });
+    res.status(200).json(blogData);
+    if(!blogData) {
+      res.status(404).json({ message: 'No blog found with this ID'});
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post('/', withAuth, async (req, res) => {
   try {
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
@@ -10,4 +11,12 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Comment };
